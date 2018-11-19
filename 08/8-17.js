68ca56479091424ab94e51454af66dca125dccd8
immi0 <saint.sohn@gmail.com>
2018-11-19 13:37:04 +0900

    chapter8 revised

--- 08/8-17.js
+++ 08/8-17.js
@@ -1,13 +1,14 @@
-// 모듈을 추출합니다.
 var express = require('express');
 
-// 서버를 생성합니다.
 var app = express();
 
-// 미들웨어를 설정합니다.
 app.use(express.static(__dirname + '/public'));
 
-// 서버를 실행합니다.
+app.use(function (request, response) {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.end('<img src="/Penguins.jpg" width="100%" />');
+});
+
 app.listen(52273, function () {
   console.log('Server running at http://127.0.0.1:52273');
 });
