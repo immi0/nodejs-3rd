3425c62642ff083d53c4231569d08f1723b42912
immi0 <saint.sohn@gmail.com>
2018-11-19 14:57:45 +0900

    Revice 08/8-16N.js

--- 08/8-16N.js
+++ 08/8-16N.js
@@ -1,14 +1,14 @@
-var http = require('http');
+//var http = require('http');
 var express = require('express');
 
 var app = express();
 
 // set middleware
-app.use(express.logger());
-app.use(app.router);
+//app.use(express.logger());  // Occured error !!! 
+//app.use(app.router);        // Occured error !!!
 app.use(express.static(__dirname + '/public'));
 
-// set router
+// set route
 app.get('/a', function (request, response) {
   response.send('<a href="/b">Go to B</a>');
 });
@@ -16,6 +16,6 @@ app.get('/b', function (request, response) {
   response.send('<a href="/a">Go to A</a>');
 })
 
-http.createServer(app).listen(52273, function () {
+app.listen(52273, function () {
   console.log('Server running at http://127.0.0.1:52273');
 });
