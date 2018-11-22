9a38b6c2eaca6e45577f499c4256ddcabfdbaae0
immi0 <saint.sohn@gmail.com>
2018-11-22 10:39:50 +0900

    chapter 9 OK

--- 09/9-7.js
+++ 09/9-7.js
@@ -1,16 +1,13 @@
-// 모듈을 추출합니다.
 var mysql = require('mysql');
 
-// 데이터베이스와 연결합니다.
 var client = mysql.createConnection({
   user: 'root',
-  password: '비밀번호',
+  password: '1',
   database: 'Company'
 });
 
-// 데이터베이스 쿼리를 실행합니다.
 client.query('INSERT INTO products (name, modelnumber, series) VALUES (?, ?, ?)', [
-    'Name Value', 'Model Number Value', 'Series Value'
+    'Example', '9_7', 'js'
 ], function (error, results, fields) {
 
 });
